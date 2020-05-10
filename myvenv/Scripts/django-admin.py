c7d8116241f4ac52bbe8587ffcca63c7bc0a68c8
yunjung <you@example.com>
2020-05-11 03:00:00 +0900

    first commit of wordcount proj

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\정윤정\desktop\정윤정\대외활동\멋쟁이사자처럼\ppp\firstdjangoproject\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
